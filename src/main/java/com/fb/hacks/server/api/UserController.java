92a1d612107b537f520571682c30790b2a78843b
Pawel Cwik <pawel.cwik@allegro.pl>
2018-05-19 22:00:33 +0200

    repair stupid bug

--- src/main/java/com/fb/hacks/server/api/UserController.java
+++ src/main/java/com/fb/hacks/server/api/UserController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
 
-    @GetMapping
-    void getUser(){
-
+    @GetMapping("/test")
+    String getUser(){
+        return "adaad";
     }
 }
