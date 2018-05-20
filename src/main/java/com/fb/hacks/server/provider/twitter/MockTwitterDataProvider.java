0891081cd8ca0f6ccfc6f38f0cd8645449538579
grzegorz <g.laganowski94@gmail.com>
2018-05-20 05:05:11 +0200

    Replace mocked words

--- src/main/java/com/fb/hacks/server/provider/twitter/MockTwitterDataProvider.java
+++ src/main/java/com/fb/hacks/server/provider/twitter/MockTwitterDataProvider.java
@@ -21,7 +21,7 @@ public class MockTwitterDataProvider implements IntegratedServiceDataProvider {
     private static Map<String, Set<String>> interests() {
         return ImmutableMap.<String, Set<String>>builder()
                 .put("greg", ImmutableSet.of("criminals", "movies", "wine"))
-                .put("john", ImmutableSet.of("programming", "obama", "beer"))
+                .put("john", ImmutableSet.of("programming", "obama", "house"))
                 .put("paul", ImmutableSet.of("trump", "football", "concerts"))
                 .put("anna", ImmutableSet.of("horses", "wine", "books"))
                 .build();
